b23c2ffa015a7b2da908dba79e01f9dd66b0078c
Vector <davidoluremi0@gmail.com>
2025-01-29 23:37:48 +0100

    Add custom purple color to Tailwind CSS theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ module.exports = {
     './src/**/*.{js,jsx,ts,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        purple: 'hsl(259, 100%, 65%)',
+      },
+    },
   },
   plugins: [],
 }
